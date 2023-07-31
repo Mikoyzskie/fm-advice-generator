cdc3476ba0b08770c270a8cad4a4abc59caca990
Mikoy <rapidstrike13@gmail.com>
2023-07-31 16:55:23 +0800

    update on alt for images

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [advice, setAdvice] = useState();
-  const [id, setId] = useState();
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState("");
 
   useEffect(() => {
     fetch("https://api.adviceslip.com/advice", {
@@ -38,18 +38,20 @@ export default function Home() {
         src={"./pattern-divider-desktop.svg"}
         width={9999}
         height={100}
+        alt="desktop-divider"
       />
       <Image
         className="md:hidden"
         src={"./pattern-divider-mobile.svg"}
         width={9999}
         height={100}
+        alt="mobile-divider"
       />
       <button
         className="dice p-4 rounded-full absolute bottom-[-25px] hover:drop-shadow-[0_0_20px_rgba(82,255,168,0.9)]"
         onClick={getAdvice}
       >
-        <Image src={"./icon-dice.svg"} width={20} height={20} />
+        <Image src={"./icon-dice.svg"} width={20} height={20} alt="dice" />
       </button>
     </main>
   );
